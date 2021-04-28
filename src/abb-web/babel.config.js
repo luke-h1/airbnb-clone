d91040f269de2e903601eea9f5fb61300bdbeefe
luke-h1 <luke.howsam@yahoo.com>
2021-04-28 21:44:10 +0100

    setup jest on server & frontend

---
+++ src/abb-web/babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    [
+      'next/babel',
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  ],
+};
