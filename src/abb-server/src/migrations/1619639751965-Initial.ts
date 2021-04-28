ae9c9aa25be435f9bcc48774e57e41b4e10cf5ef
luke-h1 <luke.howsam@yahoo.com>
2021-04-28 20:56:07 +0100

    Initial migration

--- src/abb-server/src/migrations/1619639751965-Initial.ts
+++ src/abb-server/src/migrations/1619639751965-Initial.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class Initial1619639677158 implements MigrationInterface {
-    name = 'Initial1619639677158'
+export class Initial1619639751965 implements MigrationInterface {
+    name = 'Initial1619639751965'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying NOT NULL, "password" character varying NOT NULL, "confirmed" boolean NOT NULL DEFAULT false, "forgotPasswordLocked" boolean NOT NULL DEFAULT false, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
