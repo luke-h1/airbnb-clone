ffdf11296e699f354f5ea4f0ddd2ca73a8a100da
luke-h1 <luke.howsam@yahoo.com>
2021-04-29 12:31:29 +0100

    (server): Setup more test utils

---
+++ src/abb-server/src/resolvers/user/tests/forgotPassword.test.ts
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import Redis from 'ioredis';
+import * as faker from 'faker';
+import { passwordNotLongEnough } from 'abb-common';
+import { Connection } from 'typeorm';
+import { createTestConn } from 'src/testUtils/createTestConn';
+import { User } from 'src/entities/User';
+import { TestClient } from 'src/utils/TestClient';
+
+let conn: Connection;
+export const redis = new Redis();
+faker.seed(Date.now() + 0);
+const email = faker.internet.email();
+const password = faker.internet.password();
+const newPassword = faker.internet.password();
+
+let userId: string;
+
+beforeAll(async () => {
+  conn = (await createTestConn()) as any;
+  const user = await User.create({
+    email,
+    password,
+    confirmed: true,
+  }).save();
+  userId = user.id;
+});
+
+afterAll(async () => {
+  conn.close();
+});
+
+describe('forgot password', () => {
+  test('it works', async () => {
+    const client = new TestClient(process.env.TEST_HOST);
+
+    // lock account
+  });
+});
