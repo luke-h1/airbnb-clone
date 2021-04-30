46318aa105671a87271238d51e4a9416f56e3921
luke-h1 <luke.howsam@yahoo.com>
2021-04-30 09:56:08 +0100

    (web): Finish register page styles

--- src/abb-web/src/modules/register/register.tsx
+++ src/abb-web/src/modules/register/register.tsx
@@ -1,4 +1,6 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
+import {
+  Box, Button, Flex, Link, Text,
+} from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -17,11 +19,18 @@ const index: React.FC<indexProps> = () => {
   const router = useRouter();
   const [register] = useRegisterMutation();
   return (
-    <Box as={Flex} minH="50vh" flexDirection="column" display="flex" justifyContent="center" alignItems="center">
+    <Box
+      as={Flex}
+      minH="50vh"
+      flexDirection="column"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
       <Formik
         initialValues={{ email: '', password: '' }}
         onSubmit={async (values, { setErrors }) => {
-          const res = await register({
+          const response = await register({
             variables: { options: values },
             update: (cache, { data }) => {
               cache.writeQuery<MeQuery>({
@@ -33,11 +42,13 @@ const index: React.FC<indexProps> = () => {
               });
             },
           });
-          if (res.data?.register.errors) {
-            setErrors(toErrorMap(res.data.register.errors));
-          } else if (res.data?.register.user) {
-            router.push('/');
-          }
+          console.log(response);
+          // if (response.data?.register.errors) {
+          //   console.log('response.data?.register.errors', response.data?.register.errors);
+          //   setErrors(toErrorMap(response.data.register.errors));
+          // } else if (response.data?.register.user) {
+          //   router.push('/');
+          // }
         }}
       >
         {({ isSubmitting }) => (
@@ -50,9 +61,33 @@ const index: React.FC<indexProps> = () => {
               type="password"
             />
             <Box mb={2} />
-            <Button type="submit" bg="#F93458" textColor="#fff" minW="500px">
+            <Button
+              type="submit"
+              bg="#F93458"
+              textColor="#fff"
+              minW="500px"
+              disabled={isSubmitting}
+              _hover={{
+                background: '#FE385C',
+                color: '#fff',
+              }}
+            >
               Agree and continue
             </Button>
+            <Box>
+              <Text fontSize="15px">
+                Already have an account?{' '}
+                <Text
+                  m={0}
+                  p={0}
+                  textDecoration="underline"
+                  as={Link}
+                  href="/login"
+                >
+                  Login
+                </Text>
+              </Text>
+            </Box>
           </Form>
         )}
       </Formik>
