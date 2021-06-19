1661360b2878ad95537edac5196de027105de81d
luke-h1 <luke.howsam@yahoo.com>
2021-06-19 20:20:17 +0100

    feat/add-deploy-scripts - improve server & frontend deploy formatting

--- scripts/deploy:server.sh
+++ scripts/deploy:server.sh
@@ -7,33 +7,27 @@ echo Enter user
 read USER 
 cd ../src/abb-server
 
-echo "Deploy server to development or production environment ? (enter 'dev' or 'prod')"
+echo "Deploy server to development or production? (enter 'dev' or 'prod')"
 read -r response
 if [[ $response =~ ^([dD][eE][vV])$ ]]; then
-echo "DEPLOYING BACKEND TO DEVELOPMENT ENVIRONMENT 🚀"
+echo "DEPLOYING SERVER TO DEVELOPMENT 🚀"
 
 docker build -t lhowsam/airbnb-clone-dev:$VERSION .
 docker push lhowsam/airbnb-clone-dev:$VERSION
 
 ssh ${USER}@${TARGET} -i /Users/lukehowsam/aws/*.cer "sudo docker pull lhowsam/airbnb-clone-dev:$VERSION && sudo docker tag lhowsam/airbnb-clone-dev:$VERSION dokku/airbnb-api-dev:$VERSION && dokku deploy airbnb-api-dev $VERSION"
 
-echo "SUCCESFULLY DEPLOYED BACKEND TO DEVOPMENT ✅"
+echo "SUCCESFULLY DEPLOYED SERVER TO DEV ✅"
 
 elif [[ $response =~ ^([pP][rR][oO][dD])$ ]]; then
-echo "DEPLOYING BACKEND TO PRODUCTION 😈"
+echo "DEPLOYING SERVER TO PRODUCTION 🚀"
 
 docker build -t lhowsam/airbnb-clone-prod:$VERSION .
 docker push lhowsam/airbnb-clone-prod:$VERSION
+
 ssh ${USER}@${TARGET} -i /Users/lukehowsam/aws/*.cer "sudo docker pull lhowsam/airbnb-clone-prod:$VERSION && sudo docker tag lhowsam/airbnb-clone-prod:$VERSION dokku/airbnb-api-prod:$VERSION && dokku deploy airbnb-api-prod $VERSION"
 
-echo "SUCCESFULLY DEPLOYED BACKEND TO PRODUCTION 😈"
+echo "SUCCESFULLY DEPLOYED SERVER TO PRODUCTION 🚀"
 else
-echo "Bad input, not continuing with BACKEND deploy! ❌"
+echo "Bad input, not continuing with SERVER deploy! ❌"
 fi
-
-
-
-echo "Deploying to production 🚀 🔥"
-docker build -t lhowsam/airbnb-clone:$VERSION .
-docker push lhowsam/airbnb-clone:$VERSION
-ssh ${USER}@${TARGET} -i /Users/lukehowsam/aws/*.cer "sudo docker pull lhowsam/airbnb-clone:$VERSION && sudo docker tag lhowsam/airbnb-clone:$VERSION dokku/airbnb-api:$VERSION && dokku deploy airbnb-api $VERSION"
--- scripts/deploy:web.sh
+++ scripts/deploy:web.sh
@@ -11,14 +11,17 @@ npm run tsc
 npm run build 
 vc -f 
 echo "SUCCESFULLY DEPLOYED FRONTEND TO DEV ✅"
+
 elif [[ $response =~ ^([pP][rR][oO][dD])$ ]]; then
 echo "DEPLOYING FRONTEND TO PROD"
+
 cd ../src/abb-web
 npm run format 
 npm run lint 
 npm run tsc 
 npm run build 
 vc --prod -f 
+
 echo "SUCCESFULLY DEPLOYED FRONTEND TO PRODUCTION ✅"
 else
 echo "Bad input, not continuing with frontend deploy! ❌"
