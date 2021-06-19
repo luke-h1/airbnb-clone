70db0676edee2b670146d191fa05148dc2d4825e
luke-h1 <luke.howsam@yahoo.com>
2021-06-19 12:37:26 +0100

    feat/add-property-card-component

---
+++ src/abb-web/next.config.js
@@ -0,0 +1,26 @@
+const withImages = require('next-images');
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  withImages,
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
--- src/abb-web/src/pages/_app.tsx
+++ src/abb-web/src/pages/_app.tsx
@@ -3,12 +3,14 @@ import Header from '@src/components/Header';
 import { Reset } from '@src/styles/Global';
 import '@src/styles/global.scss';
 import Footer from '@src/components/Footer';
+import PropertyCard from '@src/modules/property/components/PropertyCard';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Reset />
       <Header />
+      <PropertyCard />
       <Component {...pageProps} />
       <Footer />
     </>
