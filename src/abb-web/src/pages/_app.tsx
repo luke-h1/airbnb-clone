6f84ef1441bef8c9cf23f974551e85f8d75f6510
luke-h1 <luke.howsam@yahoo.com>
2021-06-19 13:12:32 +0100

    feat/add-property-card-component

--- src/abb-web/src/pages/_app.tsx
+++ src/abb-web/src/pages/_app.tsx
@@ -1,23 +1,18 @@
 import type { AppProps } from 'next/app';
-import Header from '@src/components/Header';
-import { Reset } from '@src/styles/Global';
 import '@src/styles/global.scss';
 import Footer from '@src/components/Footer';
-import Card from '@src/modules/property/components/Card/Card';
 import { Wrapper } from '@src/components/Wrapper';
+import Header from '@src/components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Reset />
-      <Header />
+      <Header {...pageProps} />
       <Wrapper>
-        <Card />
         <Component {...pageProps} />
       </Wrapper>
       <Footer />
     </>
   );
 }
-
 export default MyApp;
