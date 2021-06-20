6025e96c58b2e239f6c3276b03403dcecef54d40
luke-h1 <luke.howsam@yahoo.com>
2021-06-20 11:33:44 +0100

    feat/update-deploy-server-script

--- scripts/deploy:server.sh
+++ scripts/deploy:server.sh
@@ -10,24 +10,24 @@ cd ../src/abb-server
 echo "Deploy server to development or production? (enter 'dev' or 'prod')"
 read -r response
 if [[ $response =~ ^([dD][eE][vV])$ ]]; then
-echo "DEPLOYING SERVER TO DEVELOPMENT 🚀"
+echo "🚀 Deploying server to dev"
 
 docker build -t lhowsam/airbnb-clone-dev:$VERSION .
 docker push lhowsam/airbnb-clone-dev:$VERSION
 
 ssh ${USER}@${TARGET} -i /Users/lukehowsam/aws/*.cer "sudo docker pull lhowsam/airbnb-clone-dev:$VERSION && sudo docker tag lhowsam/airbnb-clone-dev:$VERSION dokku/airbnb-api-dev:$VERSION && dokku deploy airbnb-api-dev $VERSION"
 
-echo "SUCCESFULLY DEPLOYED SERVER TO DEV ✅"
+echo "✅ Succesfully deployed server to dev"
 
 elif [[ $response =~ ^([pP][rR][oO][dD])$ ]]; then
-echo "DEPLOYING SERVER TO PRODUCTION 🚀"
+echo "Deploying server to production 🚀 "
 
 docker build -t lhowsam/airbnb-clone-prod:$VERSION .
 docker push lhowsam/airbnb-clone-prod:$VERSION
 
 ssh ${USER}@${TARGET} -i /Users/lukehowsam/aws/*.cer "sudo docker pull lhowsam/airbnb-clone-prod:$VERSION && sudo docker tag lhowsam/airbnb-clone-prod:$VERSION dokku/airbnb-api-prod:$VERSION && dokku deploy airbnb-api-prod $VERSION"
 
-echo "SUCCESFULLY DEPLOYED SERVER TO PRODUCTION ✅"
+echo "✅ Succesfully deployed server to production"
 else
-echo "Bad input, not continuing with SERVER deploy! ❌"
+echo "❌ Bad input, not continuing with backend deploy!"
 fi
