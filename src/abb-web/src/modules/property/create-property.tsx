ece42349a2ba3499cad0695ed8974b9945d6082f
luke-h1 <luke.howsam@yahoo.com>
2021-06-27 16:23:43 +0100

    feat/use-maps-in-creations

--- src/abb-web/src/modules/property/create-property.tsx
+++ src/abb-web/src/modules/property/create-property.tsx
@@ -10,7 +10,7 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-const CreateMap = dynamic(() => import('./components/CreateMap2'), {
+const CreateMap = dynamic(() => import('./components/CreateMap'), {
   ssr: false,
 });
 
