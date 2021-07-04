5c8993facee05964caf8df4d7262b02296c25d3e
luke-h1 <luke.howsam@yahoo.com>
2021-07-04 12:18:32 +0100

    feat/s3-uploads - add s3.ts util

--- src/abb-server/src/shared/env.d.ts
+++ src/abb-server/src/shared/env.d.ts
@@ -14,7 +14,8 @@ declare namespace NodeJS {
     MAIL_USERNAME: string;
     MAIL_PASSWORD: string;
     AWS_ACCESS_KEY_ID: string;
-    AWS_ACCESS_KEY_SECRET: string;
+    AWS_SECRET_ACCESS_KEY: string;
+    AWS_BUCKET_NAME: string;
     AWS_BUCKET_REGION: string;
   }
 }
---
+++ src/abb-server/src/utils/image/s3.ts
@@ -0,0 +1,59 @@
+import 'dotenv-safe/config';
+import { v4 } from 'uuid';
+
+const AWS = require('aws-sdk');
+
+// set public access to s3 bucket to true (for now)
+
+AWS.config.update({
+  signatureVersion: 's3v4',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_BUCKET_REGION,
+  Bucket: process.env.AWS_BUCKET_NAME,
+});
+
+export const S3 = new AWS.S3({
+  signatureVersion: 's3v4',
+  region: process.env.AWS_BUCKET_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+const S3DefaultParams = {
+  ACL: 'public-read',
+  Bucket: process.env.AWS_BUCKET_NAME,
+  Conditions: [
+    ['content-length-range', 0, 3096000], // 3 Mb
+    { acl: 'public-read' },
+  ],
+};
+
+// actual upload to s3 happens here
+export const handleFileUpload = async (file: any) => {
+  const { createReadStream, filename } = await file;
+
+  const key = v4();
+
+  return new Promise((resolve, reject) => {
+    S3.upload(
+      {
+        ...S3DefaultParams,
+        Body: createReadStream(),
+        Key: `${key}/${filename}`,
+        Bucket: process.env.AWS_BUCKET_NAME,
+      },
+      (e, data) => {
+        if (e) {
+          console.log('error uploading...', e);
+          reject(e);
+        } else {
+          console.log('successfully uploaded file...', data);
+          resolve(data);
+        }
+      },
+    );
+  });
+};
