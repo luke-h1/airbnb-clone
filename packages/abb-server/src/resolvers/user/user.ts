b4134000f7e350572561d14d3bb919d39b475746
luke-h1 <luke.howsam@yahoo.com>
2021-07-19 22:26:49 +0100

    303 - fix server build

--- packages/abb-server/src/resolvers/user/user.ts
+++ packages/abb-server/src/resolvers/user/user.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-shadow */
-import { v4 } from 'uuid';
 import argon2 from 'argon2';
 
 // @TODO LUKE: create new upload image resolver for users
@@ -25,7 +24,6 @@ import { MyContext } from '../../shared/types';
 import { UsernamePasswordInput } from './inputs/UsernamePasswordInput';
 import { validateRegister } from '../../validation/user/validateRegister';
 import { constants } from '../../shared/constants';
-import { sendPasswordResetMail } from '../../utils/mail/sendPasswordResetMail';
 import { UserRegisterInput } from './inputs/UserRegisterInput';
 
 @ObjectType()
@@ -66,82 +64,82 @@ export class UserResolver {
     return null;
   }
 
-  @Mutation(() => UserResponse)
-  async changePassword(
-    @Arg('token') token: string,
-    @Arg('newPassword') newPassword: string,
-    @Ctx() { redis, req }: MyContext,
-  ): Promise<UserResponse> {
-    if (newPassword.length <= 2) {
-      return {
-        errors: [
-          {
-            field: 'newPassword',
-            message: 'length must be greater than 2',
-          },
-        ],
-      };
-    }
-    const key = constants.FORGET_PASSWORD_PREFIX + token;
-    const userId = await redis.get(key);
-    if (!userId) {
-      return {
-        errors: [
-          {
-            field: 'token',
-            message: 'token expired',
-          },
-        ],
-      };
-    }
-    // eslint-disable-next-line radix
-    const userIdNum = parseInt(userId);
-    const user = await User.findOne(userIdNum);
-    if (!user) {
-      return {
-        errors: [
-          {
-            field: 'token',
-            message: 'user no longer exists',
-          },
-        ],
-      };
-    }
-    await User.update(
-      { id: userIdNum },
-      {
-        password: await argon2.hash(newPassword),
-      },
-    );
-    await redis.del(key);
-    // login user after they've changed their password
-    req.session.userId = user.id;
-    return { user };
-  }
+  // @Mutation(() => UserResponse)
+  // async changePassword(
+  //   @Arg('token') token: string,
+  //   @Arg('newPassword') newPassword: string,
+  //   @Ctx() { redis, req }: MyContext,
+  // ): Promise<UserResponse> {
+  //   if (newPassword.length <= 2) {
+  //     return {
+  //       errors: [
+  //         {
+  //           field: 'newPassword',
+  //           message: 'length must be greater than 2',
+  //         },
+  //       ],
+  //     };
+  //   }
+  //   const key = constants.FORGET_PASSWORD_PREFIX + token;
+  //   const userId = await redis.get(key);
+  //   if (!userId) {
+  //     return {
+  //       errors: [
+  //         {
+  //           field: 'token',
+  //           message: 'token expired',
+  //         },
+  //       ],
+  //     };
+  //   }
+  //   // eslint-disable-next-line radix
+  //   const userIdNum = parseInt(userId);
+  //   const user = await User.findOne(userIdNum);
+  //   if (!user) {
+  //     return {
+  //       errors: [
+  //         {
+  //           field: 'token',
+  //           message: 'user no longer exists',
+  //         },
+  //       ],
+  //     };
+  //   }
+  //   await User.update(
+  //     { id: userIdNum },
+  //     {
+  //       password: await argon2.hash(newPassword),
+  //     },
+  //   );
+  //   await redis.del(key);
+  //   // login user after they've changed their password
+  //   req.session.userId = user.id;
+  //   return { user };
+  // }
 
-  @Mutation(() => Boolean)
-  async forgotPassword(
-    @Arg('email') email: string,
-    @Ctx() { redis }: MyContext,
-  ) {
-    const user = await User.findOne({ where: { email } });
-    if (!user) {
-      return true;
-    }
-    const token = v4();
+  // @Mutation(() => Boolean)
+  // async forgotPassword(
+  //   @Arg('email') email: string,
+  //   @Ctx() { redis }: MyContext,
+  // ) {
+  //   const user = await User.findOne({ where: { email } });
+  //   if (!user) {
+  //     return true;
+  //   }
+  //   const token = v4();
 
-    await redis.set(
-      constants.FORGET_PASSWORD_PREFIX + token,
-      user.id,
-      'ex',
-      1000 * 60 * 60 * 24 * 2, // 2 days to reset their password
-    );
-    await sendPasswordResetMail(
-      email,
-      `<a href="http://localhost:3000/change-password/${token}" target='_blank'>Reset Password</a>`,
-    );
-    return true;
-  }
+  //   await redis.set(
+  //     constants.FORGET_PASSWORD_PREFIX + token,
+  //     user.id,
+  //     'ex',
+  //     1000 * 60 * 60 * 24 * 2, // 2 days to reset their password
+  //   );
+  //   await sendPasswordResetMail(
+  //     email,
+  //     `<a href="http://localhost:3000/change-password/${token}" target='_blank'>Reset Password</a>`,
+  //   );
+  //   return true;
+  // }
 
   @Query(() => User, { nullable: true })
   me(@Ctx() { req }: MyContext) {
