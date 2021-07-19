9521cf3616bc6f10362d48ebef5d8bc4e5c4f582
luke-h1 <luke.howsam@yahoo.com>
2021-07-19 16:32:07 +0100

    feat/setup-rn-structure

---
+++ packages/abb-app/src/Providers.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+interface ProvidersProps {}
+
+export const Providers: React.FC<ProvidersProps> = () => {
+  return <Text>Providers go here</Text>;
+};
