f10048379641cdb82f18538d5bb60240bab72ca2
luke-h1 <luke.howsam@yahoo.com>
2021-07-31 14:50:40 +0100

    add prod mig

--- scripts/deploy:server.sh
+++ scripts/deploy:server.sh
@@ -5,23 +5,9 @@ echo Enter IP address
 read TARGET
 echo Enter user 
 read USER 
-cd ../src/abb-server
+cd ../src/api
 
-echo "Which environment do you want to deploy to ?"
-read -r response
-
-if [[ $response =~ ^([dD][eE][vV])$ ]]; then
-echo "🚀 Deploying backend to dev"
-
-docker build -t lhowsam/airbnb-clone-dev:$VERSION .
-docker push lhowsam/airbnb-clone-dev:$VERSION
-
-ssh ${USER}@${TARGET} -i /Users/lukehowsam/aws/*.cer "sudo docker pull lhowsam/airbnb-clone-dev:$VERSION && sudo docker tag lhowsam/airbnb-clone-dev:$VERSION dokku/airbnb-api-dev:$VERSION && dokku deploy airbnb-api-dev $VERSION"
-
-echo "✅ Succesfully deployed backend to dev"
-
-elif [[ $response =~ ^([pP][rR][oO][dD])$ ]]; then
-echo "🚀 Deploying backend to production"
+echo "🚀 Deploying api to production"
 
 docker build -t lhowsam/airbnb-clone-prod:$VERSION .
 docker push lhowsam/airbnb-clone-prod:$VERSION
@@ -29,6 +15,3 @@ docker push lhowsam/airbnb-clone-prod:$VERSION
 ssh ${USER}@${TARGET} -i /Users/lukehowsam/aws/*.cer "sudo docker pull lhowsam/airbnb-clone-prod:$VERSION && sudo docker tag lhowsam/airbnb-clone-prod:$VERSION dokku/airbnb-api-prod:$VERSION && dokku deploy airbnb-api-prod $VERSION"
 
 echo "✅ Succesfully deployed backend to production"
-else
-echo "❌ Bad input, not continuing with backend deploy!"
-fi
