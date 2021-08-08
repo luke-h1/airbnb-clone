e3ac6b385f54439f19cfdfc1e838762643837e18
luke-h1 <luke.howsam@yahoo.com>
2021-08-08 15:30:08 +0100

    add routes to App.tsx

--- src/client2/src/App.tsx
+++ src/client2/src/App.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
-  return <>Hello world</>;
+  return (
+    <Router>
+      {/* header */}
+      {/* wrapper */}
+      <Route path="/order/:id" component={OrderPage} />
+      <Route path="/payment" component={PaymentPage} />
+      <Route path="/placeorder" component={PlaceOrderPage} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/register" component={RegisterPage} />
+      <Route path="/profile" component={ProfilePage} />
+      <Route path="/property/:id" component={PropertyPage} />
+      <Route path="/cart/:id?" component={CartScreen} />
+      <Route path="/admin/userlist" component={UserListPage} />
+      <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+      <Route
+        path="/admin/propertylist"
+        component={PropertyListScreen}
+        exact
+      />
+      <Route
+        path="/admin/propertylist/:pageNumber"
+        component={PropertyListScreen}
+        exact
+      />
+
+      <Route path="/admin/property/:id/edit" component={PropertyEditScreen} />
+      <Route path="/admin/orderlist" component={OrderListScreen} />
+      <Route path="/search/:keyword" component={HomeScreen} exact />
+      <Route path="/page/:pageNumber" component={HomeScreen} exact />
+      <Route
+        path="/search/:keyword/page/:pageNumber"
+        component={HomeScreen}
+        exact
+      />
+      <Route path="/" component={HomeScreen} exact />
+    </Router>
+  );
 };
 
 export default App;
