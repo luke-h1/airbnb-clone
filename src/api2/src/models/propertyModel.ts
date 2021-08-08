a1bd1004716ffebe177cf10cc28d1e2f10b2d3c4
luke-h1 <luke.howsam@yahoo.com>
2021-08-08 12:36:06 +0100

    feat/rest-api (finish mongoose models)

--- src/api2/src/models/propertyModel.ts
+++ src/api2/src/models/propertyModel.ts
@@ -1,1 +1,59 @@
 import mongoose from 'mongoose';
+import reviewSchema from './reviewSchema';
+
+const propertySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    beds: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    bedrooms: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+const Property = mongoose.model('Property', propertySchema);
+export default Property;
