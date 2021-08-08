28ed2f259931f533c16391b952a3297a85c280df
luke-h1 <luke.howsam@yahoo.com>
2021-08-08 13:20:02 +0100

    feat/rest-api (finish auth middleware)

---
+++ src/api2/src/middleware/auth.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import 'dotenv-safe/config';
+
+export const protect = asyncHandler(
+  async (req: any, res: Response, next: NextFunction) => {
+    let token;
+
+    if (
+      req.headers.authorization
+      && req.headers.authorization.startsWith('Bearer')
+    ) {
+      try {
+        token = req.headers.authorization.split(' ')[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
+        //   @ts-ignore
+        req.user = await User.findById(decoded.id).select('-password');
+
+        next();
+      } catch (error) {
+        console.error(error);
+        res.status(401);
+        throw new Error('Not authorized, token failed');
+      }
+    }
+
+    if (!token) {
+      res.status(401);
+      throw new Error('Not authorized, no token');
+    }
+  },
+);
+
+export const admin = (req: any, res: Response, next: NextFunction) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+};
