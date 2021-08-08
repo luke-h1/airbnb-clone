b167177adb9de44bb697ce42bbef9ddda3e0d427
luke-h1 <luke.howsam@yahoo.com>
2021-08-08 21:23:30 +0100

    chore/fix-ci (fix remaining build errors)

--- packages/client/src/components/EditDeleteButtons.tsx
+++ packages/client/src/components/EditDeleteButtons.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 import { useDeletePropertyMutation, useMeQuery } from '@src/generated/graphql';
 import { AiFillEdit, AiTwotoneDelete } from 'react-icons/ai';
-import { Loader } from './Loader';
 
 interface EditDeleteButtonProps {
   id: number;
@@ -13,8 +12,8 @@ const EditDeleteButtons: React.FC<EditDeleteButtonProps> = ({
   id,
   creatorId,
 }) => {
-  const [deleteProperty, { loading }] = useDeletePropertyMutation();
-  const { data: meData } = useMeQuery();
+  const [, deleteProperty] = useDeletePropertyMutation();
+  const [{ data: meData }] = useMeQuery();
 
   if (meData?.me?.id !== creatorId) {
     return null;
@@ -26,25 +25,15 @@ const EditDeleteButtons: React.FC<EditDeleteButtonProps> = ({
           <AiFillEdit fontSize="23px" color="green" />
         </a>
       </Link>
-      {loading ? (
-        <div className="ml-5">
-          <Loader />
-        </div>
-      ) : (
-        <AiTwotoneDelete
-          color="red"
-          fontSize="23px"
-          onClick={() => {
-            deleteProperty({
-              variables: { id },
-              update: (cache) => {
-                // Property:23
-                cache.evict({ id: `Property:${id}` });
-              },
-            });
-          }}
-        />
-      )}
+      <AiTwotoneDelete
+        color="red"
+        fontSize="23px"
+        onClick={() => {
+          deleteProperty({
+            id,
+          });
+        }}
+      />
     </div>
   );
 };
