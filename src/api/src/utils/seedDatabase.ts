26474b133f06d518ddc4a975a2d67dbc2ad0eb8a
luke-h1 <luke.howsam@yahoo.com>
2021-08-15 15:45:18 +0100

    test/add-ui-tests - fix user creation issues

--- src/api/src/utils/seedDatabase.ts
+++ src/api/src/utils/seedDatabase.ts
@@ -1,9 +1,8 @@
 import { getConnection } from 'typeorm';
+import bcrypt from 'bcryptjs';
 import { User } from '../entities/User';
 
 export const seedDatabase = async () => {
-  //   const userExists = await User.find({ where: { email: 'bob@test.com' } });
-
   const userExists = await getConnection().query(
     `
     SELECT u.* FROM "users" u 
@@ -12,17 +11,25 @@ export const seedDatabase = async () => {
     ['bob@test.com'],
   );
 
-  if (userExists) {
-    console.log('bob the test user already exists');
+  if (!userExists) {
+    const hashedPassword = await bcrypt.hash('testpassword', 12);
+    const result = await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(User)
+      .values({
+        firstName: 'bob',
+        lastName: 'test',
+        email: 'bob@test.com',
+        password: hashedPassword,
+        image:
+          'https://images.unsplash.com/photo-1535585105038-bf206e97d068?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80',
+        imageFileName: 'testing',
+      })
+      .returning('*')
+      .execute();
+    const user = result.raw[0];
+    console.log('bob the test user has been created 😃', user);
   }
-  const user = await User.create({
-    firstName: 'bob',
-    lastName: 'test',
-    email: 'bob@test.com',
-    password: 'testpassword',
-    image:
-      'https://images.unsplash.com/photo-1535585105038-bf206e97d068?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80',
-    imageFileName: 'testing',
-  });
-  console.log('bob the test user has been created 😃', user);
+  console.log('test user already exists');
 };
