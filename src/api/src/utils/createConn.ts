34bb46994bd5da64845d1f3a102ed2a42d777d9c
luke-h1 <luke.howsam@yahoo.com>
2021-08-18 20:18:48 +0100

    feat/optimize-gql-querites (fix prod redis cookie issues (not setting cookie on client ))

--- src/api/src/utils/createConn.ts
+++ src/api/src/utils/createConn.ts
@@ -2,13 +2,12 @@ import { createConnection } from 'typeorm';
 import { join } from 'path';
 import { User } from '../entities/User';
 import { Property } from '../entities/Property';
-import { constants } from './constants';
 
 export const createConn = () => createConnection({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   migrations: [join(__dirname), './migrations/*'],
   entities: [User, Property],
-  logging: !constants.__prod__,
+  logging: true,
   synchronize: true,
 });
