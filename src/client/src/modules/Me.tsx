957e1ec3d5ad5b83ee17553383fc5f7dc5739fcf
luke-h1 <luke.howsam@yahoo.com>
2021-08-19 16:08:35 +0100

    remove follow button

--- src/client/src/modules/Me.tsx
+++ src/client/src/modules/Me.tsx
@@ -78,22 +78,6 @@ const Me = () => {
           >
             Delete account
           </Button>
-          <Button
-            flex={1}
-            fontSize="sm"
-            rounded="full"
-            bg="blue.400"
-            color="white"
-            boxShadow="0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
-            _hover={{
-              bg: 'blue.500',
-            }}
-            _focus={{
-              bg: 'blue.500',
-            }}
-          >
-            Follow
-          </Button>
         </Stack>
       </Box>
     </Center>
