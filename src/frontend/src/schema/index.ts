8903d303f87818a64d0c8ef0c2b2807a42ee7a83
luke-h1 <luke.howsam@yahoo.com>
2021-08-28 17:59:21 +0100

    add image resolver

--- src/frontend/src/schema/index.ts
+++ src/frontend/src/schema/index.ts
@@ -1,9 +1,10 @@
 import { buildSchemaSync } from 'type-graphql';
 import { authChecker } from './auth';
+import { ImageResolver } from './image';
 import { ListingResolver } from './listings';
 
 export const schema = buildSchemaSync({
-  resolvers: [ListingResolver],
+  resolvers: [ListingResolver, ImageResolver],
   emitSchemaFile: process.env.NODE_ENV === 'development',
   authChecker,
 });
