f8cd77691a26a0669611e22352f416122b28e1b6
luke-h1 <luke.howsam@yahoo.com>
2021-08-28 16:43:48 +0100

    add login & logout serverless fns

--- src/frontend/src/pages/_app.tsx
+++ src/frontend/src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css';
+import '../styles/index.css';
 import type { AppProps } from 'next/app';
+import { useApollo } from '@frontend/src/apollo';
+import { ApolloProvider } from '@apollo/client';
+import { AuthProvider } from '@frontend/src/auth/useAuth';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const client = useApollo();
+  return (
+    <AuthProvider>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
---
+++ src/frontend/src/pages/api/login.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import cookie from 'cookie';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader(
+    'Set-Cookie',
+    cookie.serialize('token', req.body.token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      maxAge: 60 * 60, // 1 hour: 60 seconds x 60 minutes = 1 hour
+      sameSite: 'strict',
+      path: '/', // available on any path in the website
+    }),
+  );
+  res.status(200).json({ ok: true });
+};
