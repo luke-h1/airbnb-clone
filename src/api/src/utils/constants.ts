cadfb18fb60375244cc8059d8f0e9b734a7fb034
luke-h1 <luke.howsam@yahoo.com>
2021-08-28 14:24:47 +0100

    feat/refactor-project (create new lerna project)

--- src/api/src/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export namespace constants {
-  export const __prod__ = process.env.NODE_ENV === 'production';
-  export const COOKIE_NAME = 'connect.sid';
-  export const FORGET_PASSWORD_PREFIX = 'forget-password:';
-  export const redisSessionPrefix = 'sess:';
-  export const userSessionIdPrefix = 'userSids:';
-  export const S3UserImageKey = 'abb-userImages';
-  export const S3PropertyImageKey = 'abb-propertyImages';
-}
